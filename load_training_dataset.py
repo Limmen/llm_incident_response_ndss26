70f2ca39a315380990408bcb3b57ae592435b785
Kim Hammar <kimham@kth.se>
2025-10-29 16:10:31 +1100

    update printout

--- load_training_dataset.py
+++ load_training_dataset.py
@@ -4,6 +4,7 @@ from transformers import AutoTokenizer
 if __name__ == '__main__':
     print("Loading training dataset.")
     dataset = load_dataset("kimhammar/CSLE-IncidentResponse-V1", data_files="examples_16_june.json")
-    instructions = dataset["train"]["instructions"][0]
-    answers = dataset["train"]["answers"][0]
-    print("Training dataset loaded successfully.")
+    instructions = list(dataset["train"]["instructions"][0])
+    answers = list(dataset["train"]["answers"][0])
+    print(f"Training dataset loaded successfully. Number of instructions: {len(instructions)}. "
+          f"Number of answers: {len(answers)}.")
